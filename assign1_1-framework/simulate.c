05091daec7909a5313bdc51b1069322b35464225
Braamling <bram.vandenakker@student.uva.nl>
2013-11-06 16:01:44 +0100

    merge fuck

--- assign1_1-framework/simulate.c
+++ assign1_1-framework/simulate.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "simulate.h"
 
@@ -14,7 +15,30 @@
 
 
 /* Add any functions you may need (like a worker) here. */
+void *wave_thread(void *a){
+	int tid =  * (int *) a;
 
+	for(int i = begin; i < end; i++){
+		if(i < 0){
+			prev = data->i_max;
+		}else{
+			prev = i;
+		}
+
+		if(i >= data->i_max){
+			next = 0;
+		}else{
+			next = i;
+		}
+
+		data->next_array[i] = (2.0F * data->current_array[i]) -
+				data->old_array[i] + 0.2 * (data->current_array[prev] - 
+				(2.0F * data->current_array[i], data->current_array[next]));
+	}
+
+
+	return NULL;
+}
 
 /*
  * Executes the entire simulation.
@@ -32,12 +56,32 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
         double *old_array, double *current_array, double *next_array)
 {
 
+	pthread_t thread_ids [ num_threads ];
+	void * results [ num_threads ];
+
+	int i;
+	for (i =0; i < num_threads ; i ++) {
+		printf("created %d\n", i);
+		pthread_create ( & thread_ids [i] , /* returned thread ids */
+				NULL , 						/* default attributes */
+				&wave_thread , 				/* start routine */
+				i ); 					/* argument */
+	}
+
+	for (i =0; i < num_threads ; i ++) {
+		pthread_join ( thread_ids [i], & results [i ]);
+		printf("joined %d\n", i);
+	}
+
+
 
 
     /*
      * After each timestep, you should swap the buffers around. Watch out none
      * of the threads actually use the buffers at that time.
      */
+
+
 
 
     /* You should return a pointer to the array with the final results. */
