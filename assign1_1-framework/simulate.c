df13e356ca7d9a02a4f69c5e9ca98bf57a5480c7
Braamling <bram.vandenakker@student.uva.nl>
2013-11-06 16:43:55 +0100

    fucking doesn't work

--- assign1_1-framework/simulate.c
+++ assign1_1-framework/simulate.c
@@ -21,16 +21,16 @@ struct Data
 
 struct Data data;
 
-struct Args
+typedef struct Args
 {
     int begin;
     int size;
     int id;
-};
+}Args;
 
 /* Add any functions you may need (like a worker) here. */
 void *wave_thread(void *a){
-	struct arguments *args = (struct arguments *)a;
+	struct Args *args = (struct Args *)a;
 	// int thread_id =  (unsigned int)pthread_self();
 
 	printf("thread id %d\n", args->id);
@@ -38,7 +38,7 @@ void *wave_thread(void *a){
 	int prev;
 	int next;
 
-	for(int i = args->start; i < (args->size + args->start); i++){
+	for(int i = args->begin; i < (args->size + args->begin); i++){
 		if(i < 0){
 			prev = data.i_max;
 		}else{
@@ -51,9 +51,9 @@ void *wave_thread(void *a){
 			next = i;
 		}
 
-		data->next_array[i] = (2.0F * data->current_array[i]) -
-				data->old_array[i] + 0.2F * (data->current_array[prev] - 
-				(2.0F * data->current_array[i], data->current_array[next]));
+		data.next_array[i] = (2.0F * data.current_array[i]) -
+				data.old_array[i] + 0.2F * (data.current_array[prev] - 
+				(2.0F * data.current_array[i], data.current_array[next]));
 	}
 
 
@@ -95,15 +95,15 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
 
     int i;
     for (i =0; i < num_threads ; i ++) {
-        struct Args arg;
+        struct Args arg= malloc (sizeof(Args));
         arg.begin = worker_size * i;
-        args.size = worker_size;
-        args.id = i;
+        arg.size = worker_size;
+        arg.id = i;
         printf("created %d\n", i);
         pthread_create ( & thread_ids [i] , /* returned thread ids */
                 NULL ,                      /* default attributes */
                 &wave_thread ,              /* start routine */
-                arg);                    /* argument */
+                &arg);                    /* argument */
     }
 
     for (i =0; i < num_threads ; i ++) {
@@ -111,8 +111,6 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
         printf("joined %d\n", i);
     }
     
-    printf("worker_size: %d\n", data.worker_size);
-
     /* You should return a pointer to the array with the final results. */
     return current_array;
 }
