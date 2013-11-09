363574c24ac3641a0500d78892bf389ace519d84
Braamling <bram.vandenakker@student.uva.nl>
2013-11-09 14:59:10 +0100

    base parallel code

--- assign1/assign1_2/assign1_2_paralel.c
+++ assign1/assign1_2/assign1_2_paralel.c
@@ -16,7 +16,7 @@ typedef struct{
     pthread_cond_t empty;
 }queue_t;
 
-// Put new value in queue.
+/* Put new value in queue. */
 void queue_enqueue(queue_t *queue, long *value){
     pthread_mutex_lock(&(queue->mutex));
     while (queue->size == queue->max_size){
@@ -25,27 +25,67 @@ void queue_enqueue(queue_t *queue, long *value){
 
 }
  
-// Get value from queue.
+/* Get value from queue. */
 long queue_dequeue(queue_t * queue){
 
 }
 
-// This is where you check if a number is divisible by the threads first 
-// inputted number. If a number is not divisible you send it to the output queue
-// which is connected with the input queue of the next thread.
+/* 
+ * This is where you check if a number is divisible by the threads first 
+ * inputted number. If a number is not divisible you send it to the output queue
+ * which is connected with the input queue of the next thread. */
 void *check_number(void *a){
-    printf("new prime number: %d", a->prime);
+    struct queue_t *inbound_queue = (struct queue_t*)a;
+
+    queue_t *outbound_queue;
+
+    long prime = queue_dequeue(inbound_queue);
+    printf("new prime number: %d", prime);
+
+    long nat_number = 0;
+    int newThread = 0;
+
+    /* Search for the first prime to initizalize child with. */
+    while(!newThread){
+        nat_number = queue_dequeue(queue);
+        if(nat_number % prime != 0){
+            newThread = 1; 
+            outbound_queue = init_queue(nat_number);
+        }
+    }
+
+    /* Init child filter. */
+    pthread_t thread;
+    pthread_create ( &thread,               /* returned thread ids */
+                    NULL ,                  /* default attributes */
+                    &check_number ,         /* start routine */
+                    &outbound_queue);       /* Que argument */
+
+    /* Keep filtering all the numbers from the previous queue. */
+    while(1){
+        nat_number = queue_dequeue(queue);
+        if(nat_number % prime != 0){
+            queue_enqueue(outbound_queue, nat_number);
+        }        
+    }
+
 }
 
 int main(int argc, char *argv[]){
     long n = 3;
     pthread_t thread_one;
+    queue_t *outbound_queue = init_queue(n);
+
+
     pthread_create ( &thread_one,           /* returned thread ids */
                     NULL ,                  /* default attributes */
-                    &check_number ,          /* start routine */
-                    // args to be further specified.);  
+                    &check_number ,         /* start routine */
+                    &outbound_queue);       /* args to be further specified. */
+    
+    /* Keep producing uneven natural numbers */
     do{
-
+        n += 2;
+        queue_enqueue(outbound_queue, n);
     }while(true)
 
 }
