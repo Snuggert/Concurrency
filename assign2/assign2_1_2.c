8439a297de2227c6f8d973fc1f9029f2f431e576
Snuggert <abe.wiersma@hotmail.nl>
2013-11-14 10:43:59 +0100

    ok

--- assign2/assign2_1_2.c
+++ assign2/assign2_1_2.c
@@ -3,7 +3,7 @@
 
 double sum(double* vec, int len){
     int i; 
-    double accu;
+    double accu = 0;
     #pragma omp parallel for critical(accu) num_threads(8) firstprivate(vec)
     for(i=0; i<len; i++){
         accu = accu + vec[i];
@@ -13,10 +13,26 @@ double sum(double* vec, int len){
 
 double reduce(double(fun)(double, double), double* vec, int len){
     int i;
-    double accu;
+    double accu = 0;
     #pragma omp parallel for critical(accu) num_threads(8) firstprivate(vec, fun)
     for(i=0; i<len; i++){
         accu = fun(accu , vec[i]);
     }
     return accu;
+}
+
+double reduce_function(double total, double reduction){
+    return total - reduction;
+}
+
+int main(int argc, char *argv[])
+{
+    double *vec;
+    vec = malloc(64 * sizeof(double));
+    for(int i=0; i<64; i++){
+        vec[i] = i;
+    }
+    printf("%g\n", sum(vec, 64));
+
+    return 0;
 }
