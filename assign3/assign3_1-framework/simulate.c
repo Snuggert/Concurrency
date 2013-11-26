8f4d037483085a99fcf03aa598db4ae23162a4d3
Snuggert <abe.wiersma@hotmail.nl>
2013-11-26 11:34:21 +0100

    It works more????

--- assign3/assign3_1-framework/simulate.c
+++ assign3/assign3_1-framework/simulate.c
@@ -29,34 +29,45 @@ double *simulate(const int i_max, const int t_max, double *old_array,
      * Your implementation should go here.
      */
     float c = 0.2;
-    double *new_array;
-    new_array = calloc(i_max + 2, sizeof(double));
-    memcpy(new_array + 1, current_array, i_max);
-    free(current_array);
-    current_array = new_array;
+    int left_index, right_index;
+    current_array = realloc(current_array, i_max + 2);
     for(int t = 0; t < t_max; t++){
         /* send to left_neighbor receive from right */
-        MPI_Send(&current_array[1], 1, MPI_DOUBLE, left_neighbor, 1,
+        MPI_Send(&current_array[0], 1, MPI_DOUBLE, left_neighbor, 1,
                 MPI_COMM_WORLD);
-        MPI_Recv(&current_array[i_max], 1, MPI_DOUBLE, right_neighbor, 1,
+        MPI_Recv(&current_array[i_max + 1], 1, MPI_DOUBLE, right_neighbor, 1,
                 MPI_COMM_WORLD, &status);
 
         /* send to right_neighbor receive from left */
         MPI_Send(&current_array[i_max - 1], 1, MPI_DOUBLE, right_neighbor, 2,
                 MPI_COMM_WORLD);
-        MPI_Recv(&current_array[0], 1, MPI_DOUBLE, left_neighbor, 2,
+        MPI_Recv(&current_array[i_max], 1, MPI_DOUBLE, left_neighbor, 2,
                 MPI_COMM_WORLD, &status);
 
         for(int i = 0; i < i_max; i++){
-            next_array[i] = (2.0 * current_array[i+1]) - old_array[i] +
-                    (c * (current_array[i] - (2.0 * current_array[i+1]) + 
-                    current_array[i+2]));
+            if(i - 1 < 0){
+                left_index = i_max;
+            }else{
+                left_index = i - 1;
+            }
+            if(i + 1 == i_max){
+                right_index = i_max + 1;
+            }else{
+                right_index = i + 1;
+            }
+
+            next_array[i] = (2.0 * current_array[i]) - old_array[i] +
+                    (c * (current_array[left_index] - (2.0 * current_array[i]) + 
+                    current_array[right_index]));
         }
         memcpy(old_array, current_array + 1, i_max);
         memcpy(current_array + 1, next_array, i_max);
     }
     memcpy(next_array, current_array + 1, i_max);
-    free(current_array);
-    free(old_array);
+    if(left_neighbor + 1 == 1){
+        for(int j = 0; j < i_max; j ++){
+            printf("Index[%d]: %g\n", j, next_array[j]);
+        }
+    }
     return next_array;    
 }
