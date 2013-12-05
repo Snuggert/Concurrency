98742e1d421ee4f6d6fa72825a246aa402cf6cab
Snuggert <abe.wiersma@hotmail.nl>
2013-12-05 12:07:15 +0100

    old stuff implementation

--- assign4/vector-add.cu
+++ assign4/vector-add.cu
@@ -2,10 +2,34 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include "timer.h"
 #include <iostream>
 
+#include "file.h"
+#include "timer.h"
+
 using namespace std;
+
+typedef double(*func_t)(double x);
+
+/*
+ * Simple gauss with mu=0, sigma^1=1
+ */
+double gauss(double x)
+{
+    return exp((-1 * x * x) / 2);
+}
+
+void fill(float *array, int offset, int range, float sample_start,
+        float sample_end, func_t f)
+{
+    int i;
+    float dx;
+
+    dx = (sample_end - sample_start) / range;
+    for (i = 0; i < range; i++) {
+        array[i + offset] = f(sample_start + i * dx);
+    }
+}
 
 /* Utility function, use to do error checking.
 
@@ -89,36 +113,97 @@ void vectorAddCuda(int n, float* a, float* b, float* result) {
 
 
 int main(int argc, char* argv[]) {
-    int n = 65536;
-    timer vectorAddTimer("vector add timer");
-    float* a = new float[n];
-    float* b = new float[n];
-    float* result = new float[n];
+    float *old, *current, *next;
+    int t_max, i_max, num_threads;
+    double time;
 
-    // initialize the vectors.
-    for(int i=0; i<n; i++) {
-        a[i] = i;
-        b[i] = i;
+    /* Parse commandline args: i_max t_max num_threads */
+    if (argc < 4) {
+        printf("Usage: %s i_max t_max num_threads [initial_data]\n", argv[0]);
+        printf(" - i_max: number of discrete amplitude points, should be >2\n");
+        printf(" - t_max: number of discrete timesteps, should be >=1\n");
+        printf(" - num_threads: number of threads to use for simulation, "
+                "should be >=1\n");
+        printf(" - initial_data: select what data should be used for the first "
+                "two generation.\n");
+        printf("   Available options are:\n");
+        printf("    * sin: one period of the sinus function at the start.\n");
+        printf("    * sinfull: entire data is filled with the sinus.\n");
+        printf("    * gauss: a single gauss-function at the start.\n");
+        printf("    * file <2 filenames>: allows you to specify a file with on "
+                "each line a float for both generations.\n");
+
+        exit(1);
+    }
+
+    i_max = atoi(argv[1]);
+    t_max = atoi(argv[2]);
+    num_threads = atoi(argv[3]);
+
+    if (i_max < 3) {
+        printf("argument error: i_max should be >2.\n");
+        exit(1);
+    }
+    if (t_max < 1) {
+        printf("argument error: t_max should be >=1.\n");
+        exit(1);
+    }
+    if (num_threads < 1) {
+        printf("argument error: num_threads should be >=1.\n");
+        exit(1);
+    }
+
+    /* Allocate and initialize buffers. */
+    old = malloc(i_max * sizeof(float));
+    current = malloc(i_max * sizeof(float));
+    next = malloc(i_max * sizeof(float));
+
+    if (old == NULL || current == NULL || next == NULL) {
+        fprintf(stderr, "Could not allocate enough memory, aborting.\n");
+        exit(1);
+    }
+
+    memset(old, 0, i_max * sizeof(float));
+    memset(current, 0, i_max * sizeof(float));
+    memset(next, 0, i_max * sizeof(float));
+
+    /* How should we will our first two generations? */
+    if (argc > 4) {
+        if (strcmp(argv[4], "sin") == 0) {
+            fill(old, 1, i_max/4, 0, 2*3.14, sin);
+            fill(current, 2, i_max/4, 0, 2*3.14, sin);
+        } else if (strcmp(argv[4], "sinfull") == 0) {
+            fill(old, 1, i_max-2, 0, 10*3.14, sin);
+            fill(current, 2, i_max-3, 0, 10*3.14, sin);
+        } else if (strcmp(argv[4], "gauss") == 0) {
+            fill(old, 1, i_max/4, -3, 3, gauss);
+            fill(current, 2, i_max/4, -3, 3, gauss);
+        } else if (strcmp(argv[4], "file") == 0) {
+            if (argc < 7) {
+                printf("No files specified!\n");
+                exit(1);
+            }
+            file_read_double_array(argv[5], old, i_max);
+            file_read_double_array(argv[6], current, i_max);
+        } else {
+            printf("Unknown initial mode: %s.\n", argv[4]);
+            exit(1);
+        }
+    } else {
+        /* Default to sinus. */
+        fill(old, 1, i_max/4, 0, 2*3.14, sin);
+        fill(current, 2, i_max/4, 0, 2*3.14, sin);
     }
 
     vectorAddTimer.start();
-    vectorAddCuda(n, a, b, result);
     vectorAddTimer.stop();
 
     cout << vectorAddTimer;
-
-    // verify the resuls
-    for(int i=0; i<n; i++) {
-        if(result[i] != 2*i) {
-            cout << "error in results! Element " << i << " is " << result[i] << ", but should be " << (2*i) << endl;
-            exit(1);
-        }
-    }
     cout << "results OK!" << endl;
             
-    delete[] a;
-    delete[] b;
-    delete[] result;
+    delete[] old;
+    delete[] current;
+    delete[] next;
     
     return 0;
 }
